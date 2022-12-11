11a0f6fd4b5c7a12ccfb54eee9603dea939c73c3
phirstchild <117740003+phirstchild@users.noreply.github.com>
2022-12-11 13:22:31 +0100

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,7 @@ int main(void)
 {
 	int n;
 	srand(time(0));
+	
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -27,5 +28,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-		return(0);
+	return(0);
 }
