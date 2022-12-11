d4c686088ec28f7f2ff13aa3e0dfb8113712c7e0
phirstchild <sulaiman.zainab79@gmail.com>
2022-12-11 10:34:54 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - returns alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int ch = 'a';
+	int CH = 'A';
+
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
