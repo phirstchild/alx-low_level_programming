9178fe1fbc227b6221218ed6abc3126ec6e2319e
phirstchild <sulaiman.zainab79@gmail.com>
2022-12-12 01:54:22 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,14 +16,14 @@ int main(void)
 		for (tens = '0'; tens <= '9'; tens++)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
-
-				if (!((ones == tens) || (tens == hundreds) || 
+			{
+				if (!((ones == tens) || (tens == hundreds) ||
 							(tens > ones) || (hundreds > tens))) /*eliminates repitition*/
 				{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && 
+					if (!(ones == '9' && hundreds == '7' &&
 								tens == '8')) /*adds commas and spaces*/
 					{
 						putchar(',');
