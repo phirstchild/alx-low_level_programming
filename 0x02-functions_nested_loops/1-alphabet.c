92046bb4cb2a9ce2e38f7f3c3d9d5f050bbd2bbf
phirstchild <117740003+phirstchild@users.noreply.github.com>
2022-12-13 23:58:50 +0100

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,20 +3,20 @@
 /**
 * print_alphabets - entry point
 *
-Return: 0
+* Return: 0
 */
 
 void print_alphabet(void)
  
 {
 char c = 'a';
- 
- while (c <= 'z')
- {    
+
+while (c <= 'z')
+ {
  _putchar(c);
  c++;
- }
-  
+}
+
  _putchar('\n');
-           
+
 }
