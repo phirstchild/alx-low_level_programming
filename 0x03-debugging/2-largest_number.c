0bb7933451f4c5db41004cd8ccbebbc946646aef
phirstchild <117740003+phirstchild@users.noreply.github.com>
2022-12-16 07:11:46 +0100

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,11 +9,12 @@
  */
 int largest_number(int a, int b, int c)
 {
- int largest;
- if (a >= b && a >= c)
- {
+int largest;
+
+if (a >= b && a >= c)
+{
 largest = a;
- }
+}
 else if (b >= a && b >= c)
 {
 largest = b;
