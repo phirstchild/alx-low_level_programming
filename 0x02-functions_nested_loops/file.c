6bb230748f3bfe73594b014e9d39b666d24aaa33
phirstchild <117740003+phirstchild@users.noreply.github.com>
2022-12-18 14:36:25 +0100

    Update file.c

--- 0x02-functions_nested_loops/file.c
+++ 0x02-functions_nested_loops/file.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 
+int main(void)
+
+  
+{
+  for (i = 0; i < 5; i++)
+  {
+    printf("today is a great day");
+    if (i < 3)
+    {
+      printf("Somebody is confused");
+    }
+    else
+    {
+      printf("I understand");
+    }
+  }
+}
+  
+
