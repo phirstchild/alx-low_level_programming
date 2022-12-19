04dd5937e902d7c2b42a7e18f85cb99b72133ff2
phirstchild <sulaiman.zainab79@gmail.com>
2022-12-18 17:14:02 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints splash
+ * @n: parameter
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j;
+
+	if (n < 0)
+	{
+		for (; i < n; i++;)
+		{
+			for (j = 0; j < 1; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n);
+		}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
+
