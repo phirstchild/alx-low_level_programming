28730ff49dab35d8e1bb0e954f831d59039a031f
phirstchild <sulaiman.zainab79@gmail.com>
2022-12-20 08:46:09 -0800

    task 4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _puts - fuction that prints a string, followed by a new line to stdout
+ * _puts - fuction that prints a string, followed by a new line, to stdout
  * @str: pointer value
  * Return: Always 0 (Success)
  */
@@ -19,4 +19,3 @@ void _puts(char *str)
 
 	_putchar('\n');
 }
-
