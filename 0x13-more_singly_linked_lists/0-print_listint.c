dc84a8297d0a49006a186c2ca98d959838586ace
phirstchild <sulaiman.zainab79@gmail.com>
2023-01-31 07:06:35 -0800

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the element of a linked list
+ * @h: pointer to the first note of a linked list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodenum = 0;
+
+	while (h)
+
+	{
+		printf("%d\n", h->n);
+		nodenum++;
+
+		h = h->next;
+
+		return (nodenum);
+	}
+
+}
